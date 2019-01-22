fe6d88248e18ca8d410ef7b40b09b9f100b0d118
Emin <eminah@code.edu.az>
2019-01-22 20:29:10 +0400

    Tik Tak Toe Game in Form App C#

---
+++ Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace TIkTakToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.RestartBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(20, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 105);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(125, 33);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 105);
+            this.button2.TabIndex = 0;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(230, 33);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(105, 105);
+            this.button3.TabIndex = 0;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(20, 136);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(105, 105);
+            this.button4.TabIndex = 0;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(125, 136);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(105, 105);
+            this.button5.TabIndex = 0;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(230, 136);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(105, 105);
+            this.button6.TabIndex = 0;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(20, 239);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(105, 105);
+            this.button7.TabIndex = 0;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(125, 239);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(105, 105);
+            this.button8.TabIndex = 0;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(230, 239);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(105, 105);
+            this.button9.TabIndex = 0;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // RestartBtn
+            // 
+            this.RestartBtn.Location = new System.Drawing.Point(355, 298);
+            this.RestartBtn.Name = "RestartBtn";
+            this.RestartBtn.Size = new System.Drawing.Size(147, 37);
+            this.RestartBtn.TabIndex = 1;
+            this.RestartBtn.Text = "Restart";
+            this.RestartBtn.UseVisualStyleBackColor = true;
+            this.RestartBtn.Click += new System.EventHandler(this.RestartBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(589, 362);
+            this.Controls.Add(this.RestartBtn);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.TikTakToe_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button RestartBtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TIkTakToe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TikTakToe_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+
+        bool tiktak = true;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+                var button = (Button)sender;
+            //int count = 0;
+            if (sender is Button)
+            {
+                if (tiktak)
+                {
+                    button.Text = "X"; 
+                    
+
+                    tiktak = false;
+                    //count++;
+                }
+                else
+                {
+                    //count = 0;
+                    button.Text = "O";
+                    tiktak = true;
+                    //count++;
+                }
+                
+            }
+
+
+
+
+            //List<Button> winnList = new List<Button>();
+
+
+            //bool @bool = true;
+            //int count = 0;
+            //foreach (var item in Controls)
+            //{
+            //    Button emin = item as Button;
+            //    if (emin is Button)
+            //    {
+            //        if (emin.Location.X == emin.Location.Y)
+            //        {
+            //            //winnList.Add(emin);
+            //            if (count == 2)
+            //            {
+            //                MessageBox.Show("You Win");
+            //                emin.Enabled = false;
+            //            }
+            //            count++;
+            //            continue;
+            //        }
+            //    }
+
+            //}
+
+            TikTak();
+
+            void TikTak()
+            {
+                if (button1.Text == "X" && button2.Text == "X" && button3.Text == "X" || button1.Text == "O" && button2.Text == "O" && button3.Text == "O")
+                {
+                    button1.BackColor = Color.Red;
+                    button2.BackColor = Color.Red;
+                    button3.BackColor = Color.Red;
+                    MessageBox.Show("You Win");
+                }
+                else if (button1.Text == "X" && button4.Text == "X" && button7.Text == "X" || button1.Text == "O" && button4.Text == "O" && button7.Text == "O")
+                {
+                    button1.BackColor = Color.Red;
+                    button4.BackColor = Color.Red;
+                    button7.BackColor = Color.Red;
+                    MessageBox.Show("You Win");
+                }
+                else if (button7.Text == "X" && button8.Text == "X" && button9.Text == "X" || button7.Text == "O" && button8.Text == "O" && button9.Text == "O")
+                {
+                    button7.BackColor = Color.Red;
+                    button8.BackColor = Color.Red;
+                    button9.BackColor = Color.Red;
+                    MessageBox.Show("You Win");
+                }
+                else if (button3.Text == "X" && button6.Text == "X" && button9.Text == "X" || button3.Text == "O" && button6.Text == "O" && button9.Text == "O")
+                {
+                    button3.BackColor = Color.Red;
+                    button6.BackColor = Color.Red;
+                    button9.BackColor = Color.Red;
+                    MessageBox.Show("You Win");
+                }
+                else if (button4.Text == "X" && button5.Text == "X" && button6.Text == "X" || button4.Text == "O" && button5.Text == "O" && button6.Text == "O")
+                {
+                    button4.BackColor = Color.Red;
+                    button5.BackColor = Color.Red;
+                    button6.BackColor = Color.Red;
+                    MessageBox.Show("You Win");
+                }
+                else if (button2.Text == "X" && button5.Text == "X" && button8.Text == "X" || button2.Text == "O" && button5.Text == "O" && button8.Text == "O")
+                {
+                    button2.BackColor = Color.Red;
+                    button5.BackColor = Color.Red;
+                    button8.BackColor = Color.Red;
+                    MessageBox.Show("You Win");
+                }
+                else if (button1.Text == "X" && button5.Text == "X" && button9.Text == "X" || button1.Text == "O" && button5.Text == "O" && button9.Text == "O")
+                {
+                    button1.BackColor = Color.Red;
+                    button5.BackColor = Color.Red;
+                    button9.BackColor = Color.Red;
+                    MessageBox.Show("You Win");
+                }
+                else if (button3.Text == "X" && button5.Text == "X" && button7.Text == "X" || button3.Text == "O" && button5.Text == "O" && button7.Text == "O")
+                {
+                    button3.BackColor = Color.Red;
+                    button5.BackColor = Color.Red;
+                    button7.BackColor = Color.Red;
+                    MessageBox.Show("You Win");
+                }
+
+            }
+
+           
+        }
+
+        private void RestartBtn_Click(object sender, EventArgs e)
+        {
+            button1.Text = " ";            
+            button2.Text = " ";
+            button3.Text = " ";
+            button4.Text = " ";
+            button5.Text = " ";
+            button6.Text = " ";
+            button7.Text = " ";
+            button8.Text = " ";
+            button9.Text = " ";
+
+        }
+    }
+}
